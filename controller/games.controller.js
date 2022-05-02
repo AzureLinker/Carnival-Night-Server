2e491982dec0f75e0427a348de05783470003303
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:18:18 +0300

    Update games.controller.js

--- controller/games.controller.js
+++ controller/games.controller.js
@@ -3,7 +3,7 @@ const db = require('../db')
 class GamesController {
     async createGames (req, res) {
         const {Name_of_the_game, Description_of_the_game, Path_to_the_directory} = req.body
-        const newGames = await db.query( `INSERT INTO Games (Name_of_the_game, Description_of_the_game, Path_to_the_directory) values ($1, $2, $3) RETURNING *` [Name_of_the_game, Description_of_the_game, Path_to_the_directory])
+        const newGames = await db.query( `INSERT INTO Games (Name_of_the_game, Description_of_the_game, Path_to_the_directory) values ($Name_of_the_game, $Description_of_the_game, $Path_to_the_directory) RETURNING *` [Name_of_the_game, Description_of_the_game, Path_to_the_directory])
         res.json (newGames.rows[0])
     }
     async getGames(req, res) {
@@ -12,17 +12,19 @@ class GamesController {
     }
     async getOneGame (req, res) {
         const idGames = req.params.id
-        const user = await db.query('SELECT * FROM Games where idGames = $1', [idGames])
+        const user = await db.query('SELECT * FROM Games where idGames = $idGames', [idGames])
         res.json(user.ress[0])
     }
     async updateGames (req, res) {
         const {idGames, Name_of_the_game, Description_of_the_game, Path_to_the_directory} = req.body
-        const user = await db.query('UPDATE Games set Name_of_the_game = $1, Description_of_the_game = $2, Path_to_the_directory = $3, idGames = $4 RETURNING *', [Name_of_the_game, Description_of_the_game, Path_to_the_directory, idGames] )
+        const user = await db.query('UPDATE Games set Name_of_the_game = $1, Description_of_the_game = $Description_of_the_game, Path_to_the_directory = $Path_to_the_directory, idGames = $idGames RETURNING *', [Name_of_the_game, Description_of_the_game, Path_to_the_directory, idGames] )
         res.json(user.rows[0])
     }
     async deleteGames (req, res) {
         const idGames = req.params.id
-        const user = await db.query('DELETE FROM Games where idUser = $1', [idGames])
+        const user = await db.query('DELETE FROM Games where idGames = $idGames', [idGames])
         res.json(user.ress[0])
     }
 }   
+
+module.exports = new GamesController();
