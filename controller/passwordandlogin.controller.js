84a37c19c30fe079439c60c7a0fb15765a85f932
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 16:57:31 +0300

    Update passwordandlogin.controller.js

--- controller/passwordandlogin.controller.js
+++ controller/passwordandlogin.controller.js
@@ -3,7 +3,7 @@ const db = require('../db')
 class PasswordAndLoginController {
     async createPasswordAndLogin (req, res) {
         const {Login, Password, ConfirmationСode, DateOfConfirmation, DateOfPasswordChange} = req.body
-        const newPasswordAndLogin = await db.query( `INSERT INTO PasswordAndLogin (Login, Password, ConfirmationСode, DateOfConfirmation, DateOfPasswordChange) values ($1, $2, $3, $4, $5) RETURNING *` [Login, Password, ConfirmationСode, DateOfConfirmation, DateOfPasswordChange])
+        const newPasswordAndLogin = await db.query( `INSERT INTO PasswordAndLogin (Login, Password, ConfirmationСode, DateOfConfirmation, DateOfPasswordChange) values ($Login, $Password, $ConfirmationСode, $DateOfConfirmation, $DateOfPasswordChange) RETURNING *` [Login, Password, ConfirmationСode, DateOfConfirmation, DateOfPasswordChange])
         res.json (newPasswordAndLogin.rows[0])
     }
     async getPasswordAndLogins(req, res) {
@@ -12,17 +12,17 @@ class PasswordAndLoginController {
     }
     async getOnePasswordAndLogin (req, res) {
         const idPasswordAndLogin = req.params.id
-        const passwordandlogin = await db.query('SELECT * FROM PasswordAndLogin where idPasswordAndLogin = $1', [idPasswordAndLogin])
+        const passwordandlogin = await db.query('SELECT * FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin', [idPasswordAndLogin])
         res.json(passwordandlogin.ress[0])
     }
     async updatePasswordAndLogin (req, res) {
         const {idPasswordAndLogin, Login, Password, ConfirmationСode} = req.body
-        const passwordandlogin = await db.query('UPDATE PasswordAndLogin set Login = $1, Password = $2, ConfirmationСode = $3 where idPasswordAndLogin = $4 RETURNING *', [Login, Password, ConfirmationСode, idPasswordAndLogin] )
+        const passwordandlogin = await db.query('UPDATE PasswordAndLogin set Login = $Login, Password = $Password, ConfirmationСode = $ConfirmationСode where idPasswordAndLogin = $idPasswordAndLogin RETURNING *', [Login, Password, ConfirmationСode, idPasswordAndLogin] )
         res.json(passwordandlogin.rows[0])
     }
     async deletePasswordAndLogin (req, res) {
         const idPasswordAndLogin = req.params.id
-        const passwordandlogin = await db.query('DELETE FROM PasswordAndLogin where idPasswordAndLogin = $1', [idPasswordAndLogin])
+        const passwordandlogin = await db.query('DELETE FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin', [idPasswordAndLogin])
         res.json(passwordandlogin.ress[0])
     }
 }   
