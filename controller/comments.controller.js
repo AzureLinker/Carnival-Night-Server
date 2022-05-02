d714e56aeac10464dbf4b90dfd5ef34cbbc32692
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:46:03 +0300

    Update comments.controller.js

--- controller/comments.controller.js
+++ controller/comments.controller.js
@@ -7,22 +7,22 @@ class CommentsController {
         res.json (newComment.rows[0])
     }
     async getComments(req, res) {
-        const comments = await db.query('SELECT * FROM Comments')
+        const comments = await db.query(`SELECT * FROM Comments`)
         res.json(comments.rows)
     }
     async getOneComment (req, res) {
         const idComments = req.params.id
-        const comments = await db.query('SELECT * FROM Comments where idComments = $1idComments, [idComments])
+        const comments = await db.query(`SELECT * FROM Comments where idComments = $1idComments`, [idComments])
         res.json(comments.ress[0])
     }
     async updateComments (req, res) {
         const {idComments, Description, Date_the_comment_was_created} = req.body
-        const comments = await db.query('UPDATE Comments set Description = $Description, Date_the_comment_was_created = $Date_the_comment_was_created where idComments = $idComments RETURNING *', [Description, Date_the_comment_was_created, idComments] )
+        const comments = await db.query(`UPDATE Comments set Description = $Description, Date_the_comment_was_created = $Date_the_comment_was_created where idComments = $idComments RETURNING *`, [Description, Date_the_comment_was_created, idComments] )
         res.json(comments.rows[0])
     }
     async deleteComments (req, res) {
         const idComments = req.params.id
-        const comments = await db.query('DELETE FROM Comments where idComments = $idComments', [idComments])
+        const comments = await db.query(`DELETE FROM Comments where idComments = $idComments`, [idComments])
         res.json(comments.ress[0])
     }
 }   
