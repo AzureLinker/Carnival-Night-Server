af02ff6d0bd513db7398a72a18025eaad22efba7
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:54:24 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -7,22 +7,22 @@ class UserController {
         res.json (newPerson.rows[0])
     }
     async getUsers(req, res) {
-        const user = await db.query('SELECT * FROM User')
+        const user = await db.query(`SELECT * FROM User`)
         res.json(user.rows)
     }
     async getOneUser (req, res) {
         const idUser = req.params.id
-        const user = await db.query('SELECT * FROM User where idUser = $idUser', [idUser])
+        const user = await db.query(`SELECT * FROM User where idUser = $idUser`, [idUser])
         res.json(user.ress[0])
     }
     async updateUser (req, res) {
         const {idUser, ThePathToTheAvatar, Nikname} = req.body
-        const user = await db.query('UPDATE User set ThePathToTheAvatar = $ThePathToTheAvatar, Nikname = $Nikname where idUser = $idUser RETURNING *', [ThePathToTheAvatar, Nikname, idUser] )
+        const user = await db.query(`UPDATE User set ThePathToTheAvatar = $ThePathToTheAvatar, Nikname = $Nikname where idUser = $idUser RETURNING *`, [ThePathToTheAvatar, Nikname, idUser] )
         res.json(user.rows[0])
     }
     async deleteUser (req, res) {
         const idUser = req.params.id
-        const user = await db.query('DELETE FROM User where idUser = $idUser', [idUser])
+        const user = await db.query(`DELETE FROM User where idUser = $idUser`, [idUser])
         res.json(user.ress[0])
     }
 }   
