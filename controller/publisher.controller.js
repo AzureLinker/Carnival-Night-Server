1f197d77d94be2f89fac75df7d6de0d643f53f82
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:53:09 +0300

    Update publisher.controller.js

--- controller/publisher.controller.js
+++ controller/publisher.controller.js
@@ -7,22 +7,22 @@ class PublisherController {
         res.json (newPublisher.rows[0])
     }
     async getPublisher(req, res) {
-        const publisher = await db.query('SELECT * FROM Publisher')
+        const publisher = await db.query(`SELECT * FROM Publisher`)
         res.json(publisher.rows)
     }
     async getOnePublishers (req, res) {
         const idPublisher = req.params.id
-        const publisher = await db.query('SELECT * FROM Publisher where idPublisher = $idPublisher', [idPublisher])
+        const publisher = await db.query(`SELECT * FROM Publisher where idPublisher = $idPublisher`, [idPublisher])
         res.json(publisher.ress[0])
     }
     async updatePublisher (req, res) {
         const {idPublisher, PublishersName} = req.body
-        const publisher = await db.query('UPDATE Publisher set ThePathToTheAvatar = $ThePathToTheAvatar where idPublisher = $idPublisher RETURNING *', [PublishersName, idPublisher] )
+        const publisher = await db.query(`UPDATE Publisher set ThePathToTheAvatar = $ThePathToTheAvatar where idPublisher = $idPublisher RETURNING *`, [PublishersName, idPublisher] )
         res.json(publisher.rows[0])
     }
     async deletePublisher (req, res) {
         const idPublisher = req.params.id
-        const publisher = await db.query('DELETE FROM Publisher where idPublisher = $idPublisher', [idPublisher])
+        const publisher = await db.query(`DELETE FROM Publisher where idPublisher = $idPublisher`, [idPublisher])
         res.json(publisher.ress[0])
     }
 }   
