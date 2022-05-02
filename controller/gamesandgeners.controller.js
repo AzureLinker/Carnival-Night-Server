cbce548835bb10d1bd7e13865e1915c385d32f21
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:47:51 +0300

    Update gamesandgeners.controller.js

--- controller/gamesandgeners.controller.js
+++ controller/gamesandgeners.controller.js
@@ -7,22 +7,22 @@ class GamesAndGenresController {
         res.json (newGamesAndGenres.rows[0])
     }
     async getGamesAndGenre(req, res) {
-        const gamesandgenres = await db.query('SELECT * FROM GamesAndGenres')
+        const gamesandgenres = await db.query(`SELECT * FROM GamesAndGenres`)
         res.json(gamesandgenres.rows)
     }
     async getOneGamesAndGenres (req, res) {
         const idGamesAndGenres = req.params.id
-        const gamesandgenres = await db.query('SELECT * FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres', [idGamesAndGenres])
+        const gamesandgenres = await db.query(`SELECT * FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres`, [idGamesAndGenres])
         res.json(gamesandgenres.ress[0])
     }
     async updateGamesAndGenres (req, res) {
         const {idGamesAndGenres, idGames, idGenres} = req.body
-        const gamesandgenres = await db.query('UPDATE GamesAndGenres set idGames = $idGames, idGenres = $idGenres where idGamesAndGenres = $idGamesAndGenres RETURNING *', [idGames, idGenres, idGamesAndGenres] )
+        const gamesandgenres = await db.query(`UPDATE GamesAndGenres set idGames = $idGames, idGenres = $idGenres where idGamesAndGenres = $idGamesAndGenres RETURNING *`, [idGames, idGenres, idGamesAndGenres] )
         res.json(gamesandgenres.rows[0])
     }
     async deleteGamesAndGenres (req, res) {
         const idGamesAndGenres = req.params.id
-        const gamesandgenres = await db.query('DELETE FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres', [idGamesAndGenres])
+        const gamesandgenres = await db.query(`DELETE FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres`, [idGamesAndGenres])
         res.json(gamesandgenres.ress[0])
     }
 }   
