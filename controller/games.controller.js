f2891c5030d0237ba9d1b17a9b62edb97a5dbbce
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:46:59 +0300

    Update games.controller.js

--- controller/games.controller.js
+++ controller/games.controller.js
@@ -7,22 +7,22 @@ class GamesController {
         res.json (newGames.rows[0])
     }
     async getGames(req, res) {
-        const user = await db.query('SELECT * FROM Games')
+        const user = await db.query(`SELECT * FROM Games`)
         res.json(user.rows)
     }
     async getOneGame (req, res) {
         const idGames = req.params.id
-        const user = await db.query('SELECT * FROM Games where idGames = $idGames', [idGames])
+        const user = await db.query(`SELECT * FROM Games where idGames = $idGames`, [idGames])
         res.json(user.ress[0])
     }
     async updateGames (req, res) {
         const {idGames, Name_of_the_game, Description_of_the_game, Path_to_the_directory} = req.body
-        const user = await db.query('UPDATE Games set Name_of_the_game = $1, Description_of_the_game = $Description_of_the_game, Path_to_the_directory = $Path_to_the_directory, idGames = $idGames RETURNING *', [Name_of_the_game, Description_of_the_game, Path_to_the_directory, idGames] )
+        const user = await db.query(`UPDATE Games set Name_of_the_game = $1, Description_of_the_game = $Description_of_the_game, Path_to_the_directory = $Path_to_the_directory, idGames = $idGames RETURNING *`, [Name_of_the_game, Description_of_the_game, Path_to_the_directory, idGames] )
         res.json(user.rows[0])
     }
     async deleteGames (req, res) {
         const idGames = req.params.id
-        const user = await db.query('DELETE FROM Games where idGames = $idGames', [idGames])
+        const user = await db.query(`DELETE FROM Games where idGames = $idGames`, [idGames])
         res.json(user.ress[0])
     }
 }   
