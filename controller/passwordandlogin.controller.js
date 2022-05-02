817e0a48533a4c6ca340d6e7805a4152031290e1
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:51:04 +0300

    Update passwordandlogin.controller.js

--- controller/passwordandlogin.controller.js
+++ controller/passwordandlogin.controller.js
@@ -7,22 +7,22 @@ class PasswordAndLoginController {
         res.json (newPasswordAndLogin.rows[0])
     }
     async getPasswordAndLogins(req, res) {
-        const passwordandlogin = await db.query('SELECT * FROM PasswordAndLogin')
+        const passwordandlogin = await db.query(`SELECT * FROM PasswordAndLogin`)
         res.json(passwordandlogin.rows)
     }
     async getOnePasswordAndLogin (req, res) {
         const idPasswordAndLogin = req.params.id
-        const passwordandlogin = await db.query('SELECT * FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin', [idPasswordAndLogin])
+        const passwordandlogin = await db.query(`SELECT * FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin`, [idPasswordAndLogin])
         res.json(passwordandlogin.ress[0])
     }
     async updatePasswordAndLogin (req, res) {
         const {idPasswordAndLogin, Login, Password, ConfirmationСode} = req.body
-        const passwordandlogin = await db.query('UPDATE PasswordAndLogin set Login = $Login, Password = $Password, ConfirmationСode = $ConfirmationСode where idPasswordAndLogin = $idPasswordAndLogin RETURNING *', [Login, Password, ConfirmationСode, idPasswordAndLogin] )
+        const passwordandlogin = await db.query(`UPDATE PasswordAndLogin set Login = $Login, Password = $Password, ConfirmationСode = $ConfirmationСode where idPasswordAndLogin = $idPasswordAndLogin RETURNING *`, [Login, Password, ConfirmationСode, idPasswordAndLogin] )
         res.json(passwordandlogin.rows[0])
     }
     async deletePasswordAndLogin (req, res) {
         const idPasswordAndLogin = req.params.id
-        const passwordandlogin = await db.query('DELETE FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin', [idPasswordAndLogin])
+        const passwordandlogin = await db.query(`DELETE FROM PasswordAndLogin where idPasswordAndLogin = $idPasswordAndLogin`, [idPasswordAndLogin])
         res.json(passwordandlogin.ress[0])
     }
 }   
