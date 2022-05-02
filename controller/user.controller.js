a34e43f9afbb6bba861842e76a35abb40a707c89
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:10:17 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,8 +2,8 @@ const db = require('../db')
 
 class UserController {
     async createUser (req, res) {
-        const {name, surname} = req.body
-        const newPerson = await db.query( `INSERT INTO User (ThePathToTheAvatar, Nikname) values ($ThePathToTheAvatar, $Nikname) RETURNING *` [ThePathToTheAvatar,Nikname])
+        const {ThePathToTheAvatar, Nikname} = req.body
+        const newPerson = await db.query( `INSERT INTO User (ThePathToTheAvatar,Nikname) values ($ThePathToTheAvatar, $Nikname) RETURNING *` [ThePathToTheAvatar,Nikname])
         res.json (newPerson.rows[0])
     }
     async getUsers(req, res) {
@@ -11,7 +11,7 @@ class UserController {
         res.json(user.rows)
     }
     async getOneUser (req, res) {
-        const idUser = req.params.id
+        const id = req.params.id
         const user = await db.query('SELECT * FROM User where idUser = $idUser', [idUser])
         res.json(user.ress[0])
     }
@@ -21,8 +21,10 @@ class UserController {
         res.json(user.rows[0])
     }
     async deleteUser (req, res) {
-        const idUser = req.params.id
+        const id = req.params.id
         const user = await db.query('DELETE FROM User where idUser = $idUser', [idUser])
         res.json(user.ress[0])
     }
 }   
+
+module.exports = new UserController();
