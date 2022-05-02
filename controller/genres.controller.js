40cb9017adb2773fafbbfd9151ee76932f63250c
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:48:45 +0300

    Update genres.controller.js

--- controller/genres.controller.js
+++ controller/genres.controller.js
@@ -7,22 +7,22 @@ class GenresController {
         res.json (newGenres.rows[0])
     }
     async getGenres(req, res) {
-        const genres = await db.query('SELECT * FROM Genres')
+        const genres = await db.query(`SELECT * FROM Genres`)
         res.json(genres.rows)
     }
     async getOneGenre (req, res) {
         const idGenres = req.params.id
-        const genres = await db.query('SELECT * FROM Genres where idGenres = $idGenres', [idGenres])
+        const genres = await db.query(`SELECT * FROM Genres where idGenres = $idGenres`, [idGenres])
         res.json(genres.ress[0])
     }
     async updateGenres (req, res) {
         const {idGenres, Name_of_the_genre} = req.body
-        const genres = await db.query('UPDATE Genres set Name_of_the_genre = $Name_of_the_genre where idGenres = $idGenres RETURNING *', [Name_of_the_genre, idGenres] )
+        const genres = await db.query(`UPDATE Genres set Name_of_the_genre = $Name_of_the_genre where idGenres = $idGenres RETURNING *`, [Name_of_the_genre, idGenres] )
         res.json(genres.rows[0])
     }
     async deleteGenres (req, res) {
         const idGenres = req.params.id
-        const genres = await db.query('DELETE FROM Genres where idGenres = $idGenres', [idGenres])
+        const genres = await db.query(`DELETE FROM Genres where idGenres = $idGenres`, [idGenres])
         res.json(genres.ress[0])
     }
 }   
