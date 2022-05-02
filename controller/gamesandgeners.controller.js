72e4999cb654f1ef0308e371cc07637a255d932a
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 17:42:23 +0300

    Add files via upload

---
+++ controller/gamesandgeners.controller.js
@@ -0,0 +1,30 @@
+const db = require('../db')
+
+class GamesAndGenresController {
+    async createUser (req, res) {
+        const {idGames, idGenres} = req.body
+        const newGamesAndGenres = await db.query( `INSERT INTO GamesAndGenres (idGames, idGenres) values ($idGames, $idGenres) RETURNING *` [idGames, idGenres])
+        res.json (newGamesAndGenres.rows[0])
+    }
+    async getGamesAndGenre(req, res) {
+        const gamesandgenres = await db.query('SELECT * FROM GamesAndGenres')
+        res.json(gamesandgenres.rows)
+    }
+    async getOneGamesAndGenres (req, res) {
+        const idGamesAndGenres = req.params.id
+        const gamesandgenres = await db.query('SELECT * FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres', [idGamesAndGenres])
+        res.json(gamesandgenres.ress[0])
+    }
+    async updateGamesAndGenres (req, res) {
+        const {idGamesAndGenres, idGames, idGenres} = req.body
+        const gamesandgenres = await db.query('UPDATE GamesAndGenres set idGames = $idGames, idGenres = $idGenres where idGamesAndGenres = $idGamesAndGenres RETURNING *', [idGames, idGenres, idGamesAndGenres] )
+        res.json(gamesandgenres.rows[0])
+    }
+    async deleteGamesAndGenres (req, res) {
+        const idGamesAndGenres = req.params.id
+        const gamesandgenres = await db.query('DELETE FROM GamesAndGenres where idGamesAndGenres = $idGamesAndGenres', [idGamesAndGenres])
+        res.json(gamesandgenres.ress[0])
+    }
+}   
+
+module.exports = new GamesAndGenresController();
