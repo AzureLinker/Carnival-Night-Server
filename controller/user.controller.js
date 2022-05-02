3057a0b39f91d6cfe1d305e993c112ce103a0fb1
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-02 16:52:50 +0300

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,7 +3,7 @@ const db = require('../db')
 class UserController {
     async createUser (req, res) {
         const {name, surname} = req.body
-        const newPerson = await db.query( `INSERT INTO User (ThePathToTheAvatar,Nikname) values ($1, $2) RETURNING *` [ThePathToTheAvatar,Nikname])
+        const newPerson = await db.query( `INSERT INTO User (ThePathToTheAvatar, Nikname) values ($ThePathToTheAvatar, $Nikname) RETURNING *` [ThePathToTheAvatar,Nikname])
         res.json (newPerson.rows[0])
     }
     async getUsers(req, res) {
@@ -12,17 +12,17 @@ class UserController {
     }
     async getOneUser (req, res) {
         const idUser = req.params.id
-        const user = await db.query('SELECT * FROM User where idUser = $1', [idUser])
+        const user = await db.query('SELECT * FROM User where idUser = $idUser', [idUser])
         res.json(user.ress[0])
     }
     async updateUser (req, res) {
         const {idUser, ThePathToTheAvatar, Nikname} = req.body
-        const user = await db.query('UPDATE User set ThePathToTheAvatar = $1, Nikname = $2 where idUser = $3 RETURNING *', [ThePathToTheAvatar, Nikname, idUser] )
+        const user = await db.query('UPDATE User set ThePathToTheAvatar = $ThePathToTheAvatar, Nikname = $Nikname where idUser = $idUser RETURNING *', [ThePathToTheAvatar, Nikname, idUser] )
         res.json(user.rows[0])
     }
     async deleteUser (req, res) {
         const idUser = req.params.id
-        const user = await db.query('DELETE FROM User where idUser = $1', [idUser])
+        const user = await db.query('DELETE FROM User where idUser = $idUser', [idUser])
         res.json(user.ress[0])
     }
 }   
