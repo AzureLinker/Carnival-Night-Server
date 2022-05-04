1effa3aa00ff0390cf56feefe6be7d6228fba41f
AniriT1 <90499221+AniriT1@users.noreply.github.com>
2022-05-04 17:36:33 +0300

    Update gamesandgeners.controller.js

--- controller/gamesandgeners.controller.js
+++ controller/gamesandgeners.controller.js
@@ -3,7 +3,7 @@ const db = require('../db')
 class GamesAndGenresController {
     async createGamesAndGenres (req, res) {
         const {idGames, idGenres} = req.body
-        const newGamesAndGenres = await db.query( `INSERT INTO GamesAndGenres (idGames, idGenres) values ($idGames, $idGenres) RETURNING *` [idGames, idGenres])
+        const newGamesAndGenres = await db.query( `INSERT INTO GamesAndGenres (idGames, idGenres) values ($idGames, $idGenres) RETURNING *`, [idGames, idGenres])
         res.json (newGamesAndGenres.rows[0])
     }
     async getGamesAndGenre(req, res) {
